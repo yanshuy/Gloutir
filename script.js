b10fc05219bf3089ec21c713d7e66678e1f5cd35
Yanshuman_Yadav <yanshuman2005@gmail.com>
2024-03-02 02:32:37 +0530

    slider

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ function removeJello() {
 logo.addEventListener("mouseover", addJello);
 
 logo.addEventListener("animationend", removeJello);
+
+document.addEventListener("click", (e) => {
+  const sliderArrow = e.target.closest(".slider-arrow");
+  // console.log(sliderArrow);
+  if (sliderArrow != null) moveSlider(sliderArrow);
+});
+
+function moveSlider(sliderArrow) {
+  const slider = sliderArrow
+    .closest(".section3-imgbox")
+    .querySelector(".slider");
+  const sliderIndex = parseInt(
+    getComputedStyle(slider).getPropertyValue("--slider-index")
+  );
+  if (sliderArrow.classList.contains("left-arrow-button")) {
+    slider.style.setProperty("--slider-index", sliderIndex + 1);
+  }
+  if (sliderArrow.classList.contains("right-arrow-button")) {
+    slider.style.setProperty("--slider-index", sliderIndex - 1);
+  }
+}
