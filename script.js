d11aca8af5371ae086a476f4e4e06cefc2ebcec9
Yanshuman_Yadav <yanshuman2005@gmail.com>
2024-03-17 12:50:38 +0530

    animations

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
 const element = document.querySelector(".animation-element");
+// const donutBounce = document.querySelector(".donutBounce");
 const observer = new IntersectionObserver((entries) => {
   element.classList.toggle("in-view", entries[0].isIntersecting);
 });
 
+const observer2 = new IntersectionObserver((entries) => {
+  element.classList.toggle("donutBounce", entries[0].isIntersecting);
+  const donutBounce = document.querySelector(".donutBounce");
+  donutBounce.animate(
+    [
+      {
+        transform: "translate3d(0, -1000px, 0) scaleX(1.3)",
+        offset: 0,
+        opacity: 1,
+      },
+
+      {
+        transform: "translate3d(0, 0px, 0) scaleX(1)",
+        offset: 0.33,
+      },
+
+      {
+        transform: "translate3d(0, -90px, 0) scaleX(0.92)",
+        offset: 0.5,
+      },
+
+      {
+        transform: "translate3d(0, 0px, 0) scaleX(1)",
+        offset: 0.666,
+      },
+
+      {
+        transform: "translate3d(0, -30px, 0) scaleX(0.96)",
+        offset: 0.8333,
+      },
+
+      {
+        transform: "translate3d(0, 0px, 0) scaleX(1)",
+        offset: 1,
+        opacity: 1,
+      },
+    ],
+    {
+      easing: "ease-in",
+      duration: 1250,
+      fill: "none",
+    }
+  );
+  element.classList.remove("donutBounce");
+  observer2.unobserve(element);
+});
+
 observer.observe(element);
+observer2.observe(element);
 
 const logo = document.querySelector(".Gloutir");
 
@@ -46,3 +95,15 @@ function moveSlider(sliderArrow) {
     }
   }
 }
+
+const openModal = document.getElementById("big-button");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close-modal");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
